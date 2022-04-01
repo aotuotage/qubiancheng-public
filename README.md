bbf714ca1347195e1507fd323b6bda90d5102194
aotuotage <826931037@qq.com>
2022-04-01 20:54:03 +0800

    push one

---
+++ README.md
@@ -0,0 +1,14 @@
+# qubiancheng1024
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# 依赖安装
+npm install
+
+# 热更新
+nodemon app
+
+#说明：redis.js文件暂时没用到。mangodb读写插件mongoose。token生成验证插件jwt。
--- token/tokenObj.js
+++ token/tokenObj.js
@@ -5,14 +5,14 @@ var tokenObj = {
         var content = {
             name:cont
         } //token存储的内容
-        var secretOrPrivateKey = 'qubiancheng1024.com' // 这是加密的key（密钥或私钥） 
+        var secretOrPrivateKey = '123654' // 这是加密的key（密钥或私钥） 
         var token = jwt.sign(content, secretOrPrivateKey, {
             expiresIn: time // 过期时间
         })
         return token;
     },
     verifyToken: function (token, fn) {//验证token
-        var secretOrPrivateKey = 'qubiancheng1024.com' // 这是加密的key（密钥或私钥）
+        var secretOrPrivateKey = '123654' // 这是加密的key（密钥或私钥）
         jwt.verify(token, secretOrPrivateKey, function (err, decode) {
             if (err) { // 当token过期，或这是一个伪造的token，或这是无效的token时会触发此逻辑 
                 console.log(err)
